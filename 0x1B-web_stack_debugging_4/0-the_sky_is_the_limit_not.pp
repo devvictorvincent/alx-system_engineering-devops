907f4b655a6ad317fee14d2a76c95da41f6b0d4b
Vincent Victor <dev.victorvincent@gmail.com>
2024-01-28 04:18:50 +0000

    fixing puppet-lint issues

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,16 +2,16 @@
 
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-	# Modify the ULIMIT value
-	command => 'sed -i "s/15/4096/" /etc/default/nginx',
-	# Specify the path of the sed command
-	path    => '/usr/local/bin/:/bin/'
+  # Modify the ULIMIT value
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  # Specify the path of the sed command
+  path    => '/usr/local/bin/:/bin/'
 }
 
 # Restart Nginx
 exec { 'nginx-restart':
-	# Restart Nginx Service
-	command => 'nginx restart',
-	# specify the path for the init.d script
-	path    => '/etc/init.d/'
+  # Restart Nginx Service
+  command => 'nginx restart',
+  # specify the path for the init.d script
+  path    => '/etc/init.d/'
 }
