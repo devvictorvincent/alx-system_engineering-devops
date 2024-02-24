9320017e49bcf5c50e00a1e3ec0dc4f34dd8269b
Vincent Victor <dev.victorvincent@gmail.com>
2024-02-23 23:50:02 -0800

    get started

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that queries the subscribers on a given Reddit subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    myheaders = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=myheaders, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
